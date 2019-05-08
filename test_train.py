5e5cf1b11d91c7bab6459ac1b11efbcd0f60f9e3
Tyson McLeod <tyson.mcleod.8851@student.uu.se>
2019-05-08 22:36:59 +0200

    plz

--- test_train.py
+++ test_train.py
@@ -35,11 +35,40 @@ X = X/255.0
 # build the model
 # flatten transforms the 2d array to a 1d array
 model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(120, 120,1)),
+
+    ## c1
+    keras.layers.Conv2D(64, (3,3), input_shape=(120,120,1)),
+    keras.layers.Activation("relu"),
+    keras.layers.MaxPooling2D(pool_size=(3,3), strides=(2,2)),
+    keras.layers.Dropout(0.2),
+
+    ## c2
+    keras.layers.Conv2D(128, (3,3)),
+    keras.layers.Activation("relu"),
+    keras.layers.MaxPooling2D(pool_size=(2,2), strides=(2,2)),
+    keras.layers.Dropout(0.2),
+
+    ## c3
+    keras.layers.Conv2D(128, (3,3)),
+    keras.layers.Activation("relu"),
+    keras.layers.MaxPooling2D(pool_size=(2,2), strides=(2,2)),
+    keras.layers.Dropout(0.2),
+
+    ## c4
+    keras.layers.Conv2D(64, (3,3)),
+    keras.layers.Activation("relu"),
+    keras.layers.MaxPooling2D(pool_size=(2,2), strides=(2,2)),
+    keras.layers.Dropout(0.2),
+
+    # flatten for dense layers
+    keras.layers.Flatten(),
     keras.layers.Dense(128, activation=tf.nn.relu),
     keras.layers.Dense(10, activation=tf.nn.softmax)
+
+
 ])
 
+# SAVE BEST MODEL KERAS --> REMEMBER
 
 ### compile the model: add loss function -> what we want to minimize
 ### add optimizer -> how the model is updated based on the data it sees and its loss print_function
@@ -49,36 +78,19 @@ model.compile(optimizer ='adam',
               loss = 'sparse_categorical_crossentropy',
               metrics = ['accuracy'])
 
-'''
-model = Sequential()
+# model summary
+model.summary()
 
-model.add(Conv2D(256, (3,3), input_shape = (X.shape[1:])))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2,2)))
+## early stopping
+# monitor val_loss , patience min number of epochs to wait before , mode (auto/min) what monitored
+earlyStopping = keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=8, verbose=0, mode='auto')
 
-model.add(Conv2D(256, (3,3)))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2,2)))
-
-model.add(Flatten())
-model.add(Dense(64))
-
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
-
-model.compile(optimizer ='adam',
-              loss = 'sparse_categorical_crossentropy',
-              metrics = ['accuracy'])
-
-'''
-### compile the model: add loss function -> what we want to minimize
-### add optimizer -> how the model is updated based on the data it sees and its loss print_function
-### metrics -> used to monitor training and testing steps, this uses the fraction of images that are correctly classifeid
-
+## model checkpoint
+#
+#mcSave = keras.callbacks.ModelCheckpoint('weights.{epochs:02d}.{val_loss:.2f}.hdf5', monitor='val_loss', verbose=0, save_best_only = True, mode='auto', period= 1)
 
 ## train with model.fit
-
-model.fit(X, y, batch_size = 32, epochs=10, validation_split= 0.3)
+model.fit(X, y, batch_size = 32, epochs=10, callbacks=[earlyStopping],validation_split= 0.3)
 
 ## evaluate the accuracy
 #test_loss, test_acc = model.evaluate(y_train, y_test)
